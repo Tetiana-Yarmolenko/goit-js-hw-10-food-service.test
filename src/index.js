f5cd681847bea08a5635f0ec8b311318a4b6d786
Tetiana-Yarmolenko <kylik_t_86_86@ukr.net>
2020-10-28 15:07:45 +0200

    last

--- src/index.js
+++ src/index.js
@@ -1,5 +1,52 @@
 
 import './styles.css';
+import itemsColection from './menu.json';
+import cardTpl from "../template.hbs";
 
+const cardsMarkup = createCardsMarkup(itemsColection);
+
+const menu = document.querySelector('.js-menu');
+const switchBtn = document.querySelector('.theme-switch__toggle');
+
+// додаємо розмітку а html
+menu.insertAdjacentHTML('beforeend', cardsMarkup);
+// змінюємо тему сторінки
+switchBtn.addEventListener("change", onCheckBoxClick);
+
+// функ. для створення розмітки
+function createCardsMarkup(item) {
+    return cardTpl(item);
+}
+
+
+const Theme = {
+  LIGHT: "light-theme",
+  DARK: "dark-theme"
+};
+;
+
+// функія для зміни теми на сторінці
+function onCheckBoxClick() {
+    if (switchBtn.checked) {
+        document.body.classList.add(Theme.DARK);
+        document.body.classList.remove(Theme.LIGHT);
+        localStorage.setItem("theme", Theme.DARK);
+    }
+    else {
+        document.body.classList.add(Theme.LIGHT);
+        document.body.classList.remove(Theme.DARK);
+        localStorage.setItem("theme", Theme.LIGHT);
+    }
+}
+
+// збереження теми сторн. при перезавантаженні
+const themeLocalStorage = localStorage.getItem('theme');
+if (themeLocalStorage === Theme.DARK) {
+  document.body.classList.add(Theme.DARK);
+  switchBtn.checked = true;
+}
+  else {
+    document.body.classList.add(Theme.LIGHT);
+  }
 
 
